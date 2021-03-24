32469690b3f8394228c538d3f54210511d3062d2
Hendrik Timm <hendrik-timm@hotmail.de>
2021-03-24 18:12:07 +0100

    copied working repository

---
+++ test.py
@@ -0,0 +1,29 @@
+import sys
+
+# -*- coding: utf-16 -*-
+print("｡☆✼★━━━━━━━━━━━━★✼☆｡")
+unicode_string = "\u2665\u00C4\u00C6"
+print(u"\u2605")
+print("star" , "\u2605")
+
+print(unicode_string)
+print("hearts ",'\u2665')
+print('\u2662')
+print("clubs", '\u2663')
+print('\u2664')
+print("diamond: ", '\u2666')
+print('\u2667')
+print("spades", '\u2660')
+print("crossed swords", '\u2694')
+print (sys.getdefaultencoding())
+import sys
+print (sys.stdout.encoding)
+print (sys.getfilesystemencoding())
+#include <stdio.h>
+#include <io.h>
+#include <fcntl.h>
+
+
+
+print("░▒▓│┤╡╢╖╕╣║╗╝╜╛┐└┴┬├─┼╞╟╚╔╩╦╠═╬╧╨╤╥╙╘╒╓╫╪┘┌█▄▌▐▀αßΓπΣσµτΦΘΩδ∞φε∩≡±\n");
+print("\u0518\n"); # CYRILLIC CAPITAL LETTER YAE U+0518
